40a30904678b7e48de3955aff719c317b0841178
Tushar Khari <tusharkhari.seraphic@gmail.com>
2024-09-01 12:06:18 +0530

    setup

--- lib/utils/routes/app_routes_names.dart
+++ lib/utils/routes/app_routes_names.dart
@@ -1,21 +1,14 @@
-
-
-
-
-
 class AppRouteNames {
-  static final AppRouteNames _AppRouteNames = AppRouteNames._internal();
-  
+  static final AppRouteNames _appRouteNames = AppRouteNames._internal();
+
   factory AppRouteNames() {
-    return _AppRouteNames;
+    return _appRouteNames;
   }
   AppRouteNames._internal();
 
-
   // ===================
 
-String loginScreen = "/LoginScreen";
-String otpVerificationScreen = "/OtpVerificationScreen";
-String bottomNavBar = "/BottomNavBar";
-
+  String loginScreen = "/LoginScreen";
+  String otpVerificationScreen = "/OtpVerificationScreen";
+  String bottomNavBar = "/BottomNavBar";
 }
